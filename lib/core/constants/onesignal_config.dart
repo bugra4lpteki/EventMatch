5efaa2c50fdac164772990fb5c7341473abe14c1
bugra4lpteki <bugra4lpteki@gmail.com>
2026-09-08 02:51:13 +0300

    feat: OneSignal gercek App ID tanimlandi

--- lib/core/constants/onesignal_config.dart
+++ lib/core/constants/onesignal_config.dart
@@ -3,6 +3,6 @@ class OneSignalConfig {
   /// https://onesignal.com dashboard -> Settings -> Keys & IDs sayfasındaki App ID
   static const String appId = String.fromEnvironment(
     'ONESIGNAL_APP_ID',
-    defaultValue: 'd2bb525e-333e-48a0-bb65-8b3e34b92b6a', // Varsayılan veya env App ID
+    defaultValue: 'bc0c0b94-e465-4b0f-b01c-581d848df2ca',
   );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,6 @@ import app_links
 import audioplayers_darwin
 import file_picker
 import file_selector_macos
-import firebase_core
-import firebase_messaging
 import flutter_local_notifications
 import geolocator_apple
 import google_sign_in_ios
@@ -24,8 +22,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
